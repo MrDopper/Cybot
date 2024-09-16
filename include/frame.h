de6c90b2f8febee8039d806ab34ec9ad4f656c6d
MrDopper <145393758+MrDopper@users.noreply.github.com>
2024-09-16 12:54:47 -0700

    I Created a group box for sprite sheet

---
+++ include/frame.h
@@ -0,0 +1,15 @@
+#ifndef FRAME_H
+#define FRAME_H
+#include <iostream>
+#include <list>
+#include <fstream>
+#include <sstream>
+#include <SDL2/SDL.h>
+#include "drawing_functions.h"
+#include "Globals.h"
+
+class Frame{
+
+};
+
+#endif
